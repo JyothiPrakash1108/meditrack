cad6d35f6a9e2e306fa2b8e62fbca289f732c004
jyothiprakash <jyothiprakash1108@gmail.com>
2025-12-14 15:35:22 +0530

    add patient service

--- src/main/java/com/example/meditrack/service/PatientService.java
+++ src/main/java/com/example/meditrack/service/PatientService.java
@@ -1,5 +1,18 @@
 package com.example.meditrack.service;
 
-public class PatientService {
+import com.example.meditrack.entity.Patient;
+import com.example.meditrack.interfaces.IdStrategy;
+import com.example.meditrack.util.DataStore;
+import com.example.meditrack.util.Validator;
 
+public class PatientService {
+    private DataStore<Patient> patientStore;
+    private IdStrategy idStrategy ;
+    public Patient createPatient(String name,int age,String phoneNumber,String email,String medicalPrescription){
+        String id = idStrategy.generate();
+        Validator.vali
+        Patient createdPatient = new Patient(id, name, age, phoneNumber, email,medicalPrescription);
+        patientStore.add(id, createdPatient);
+        return createdPatient;
+    }
 }
