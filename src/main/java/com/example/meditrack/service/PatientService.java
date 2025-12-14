2198626086740e36ed7ad2f06f25092757a1fe23
jyothiprakash <jyothiprakash1108@gmail.com>
2025-12-14 19:44:09 +0530

    add searchPatient

--- src/main/java/com/example/meditrack/service/PatientService.java
+++ src/main/java/com/example/meditrack/service/PatientService.java
@@ -1,5 +1,9 @@
 package com.example.meditrack.service;
 
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Optional;
+
 import com.example.meditrack.entity.Patient;
 import com.example.meditrack.interfaces.IdStrategy;
 import com.example.meditrack.util.DataStore;
@@ -8,11 +12,27 @@ import com.example.meditrack.util.Validator;
 public class PatientService {
     private DataStore<Patient> patientStore;
     private IdStrategy idStrategy ;
-    public Patient createPatient(String name,int age,String phoneNumber,String email,String medicalPrescription){
+    public 
+    public Patient addPatient(String name,int age,String phoneNumber,String email,String medicalPrescription){
         String id = idStrategy.generate();
-        Validator.vali
+        Validator.validateAge(age);
+        Validator.validateEmail(email);
+        Validator.validateName(name);
+        Validator.validatePhoneNumber(phoneNumber);
         Patient createdPatient = new Patient(id, name, age, phoneNumber, email,medicalPrescription);
         patientStore.add(id, createdPatient);
         return createdPatient;
+    }
+    public Optional<Patient> searchPatient(String id){
+        return patientStore.getById(id);
+    }
+    public List<Patient> searchPatient(String name){
+        Validator.validateName(name);
+        List<Patient> result = new ArrayList<>();
+        for(Patient patient : patientStore.getAll()){
+            if (patient.getName().equalsIgnoreCase(name)) {
+            result.add(patient);
+        }
+        return result;
     }
 }
